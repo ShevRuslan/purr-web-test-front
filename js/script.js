bf417001766c5759d651ad40d32d24416585c784
Ruslan <sasa02sasa@mail.ru>
2022-04-28 02:45:41 +0600

    add validation form

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ buttonAcceptCookie.addEventListener("click", () => {
     cookieBlock.remove();
   }, 500);
 });
+
+const buttonSend = document.querySelector(".get-in-touch-form__button");
+const inputs = document.querySelectorAll(".get-in-touch-form__input");
+
+buttonSend.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputs.forEach((input) => {
+    if (input.value.trim() == "") {
+      input.style.borderColor = "#FF0000";
+    }
+  });
+});
